f9bb9ab228e5278670a65323c948a6f303411ea7
pray-lee <praycis@gmail.com>
2018-09-20 22:33:38 +0800

    add varible

--- print.py
+++ print.py
@@ -4,4 +4,13 @@
 # python2 3区别：
 # python默认编码方式ascII码，python3默认编码方式UTF-8，如果python2程序有中文，需要在文件首行加一句'#-*- encoding:utf-8 -*-'
 
+# 变量规范
+#   必须由数字，字母，下划线任意组合，且不能数字开头
+#   不能是python中的关键字
+#   ['and', 'as', 'assert', 'break', 'class', 'continue',
+# 	'def', 'del', 'elif', 'else', 'except', 'exec',
+# 	'finally', 'for', 'from', 'global', 'if', 'import',
+# 	'in', 'is', 'lambda', 'not', 'or', 'pass', 'print',
+# 	'raise', 'return', 'try', 'while', 'with', 'yield']
+
 
