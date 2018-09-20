1fba226e467b1e574b3a957ddb919e5aaecff4ca
pray-lee <praycis@gmail.com>
2018-09-20 23:57:50 +0800

    update 循环

--- 循环.py
+++ 循环.py
@@ -2,9 +2,14 @@
 # 循环
 flag = True
 count = 1
-
+'''
 while flag:
     print(count)
     count = count + 1
     if count > 100:
         flag = False
+
+while count <= 100:
+    print(count)
+    count = count + 1
+'''
