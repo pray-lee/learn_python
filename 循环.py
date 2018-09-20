06a4f8a0fc18fb2c0440b183c715439facdf77a6
pray-lee <praycis@gmail.com>
2018-09-20 23:54:28 +0800

    add if and while

---
+++ 循环.py
@@ -0,0 +1,10 @@
+# -*- encoding:utf-8 -*-
+# 循环
+flag = True
+count = 1
+
+while flag:
+    print(count)
+    count = count + 1
+    if count > 100:
+        flag = False
--- 条件语句.py
+++ 条件语句.py
@@ -16,11 +16,14 @@ else:
 """
 
 # 多选
-num = input('请输入您猜的数字:')
+num = str(input('请输入您猜的数字:'))
 
-if num == 1:
+if num == '1':
     print('您猜的是1')
-elif num == 2:
+elif num == '2':
     print('您猜的是2')
 else:
     print('您猜的是别的')
+
+# 字符串转数字 int('1234') <1234>
+# 数字转字符串 str(1234)   <'1234'>
