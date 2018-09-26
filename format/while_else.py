52bc93f6363ddfee19f13582a6cb9dda717a5ef2
pray-lee <praycis@gmail.com>
2018-09-26 12:24:15 +0800

    add while-else

---
+++ format/while_else.py
@@ -0,0 +1,9 @@
+count = 0
+while count <=5:
+    count += 1
+    if count == 3:
+        break
+    print('loop', count)
+else:
+    print('循环正常执行')
+# 如果while循环被break打断，后面的else不会走，如果没有被break打断，else里面的语句会正常执行
