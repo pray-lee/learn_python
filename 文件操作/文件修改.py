26801a646422e9d00d6248f599ca345c0861ca79
pray-lee <praycis@gmail.com>
2018-11-12 01:02:45 +0800

    文件修改

---
+++ 文件操作/文件修改.py
@@ -0,0 +1,10 @@
+with open('./修改前.txt', encoding="utf-8") as f, open('./修改后.bak', 'w', encoding="utf-8") as f1:
+  for line in f:
+    if '李晓勇' in line:
+      line = line.replace('李晓勇','李雨鸿')
+      print(line)
+    f1.write(line)
+
+import os
+os.remove('修改前.txt')
+os.rename('修改后.bak', '修改前.txt')
