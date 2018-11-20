34cf368d63bfd819b8341c3615c7fd58030b4e6a
pray-lee <praycis@gmail.com>
2018-11-20 15:06:25 +0800

    update

--- 递归函数/二分查找.js
+++ 递归函数/二分查找.js
@@ -1,11 +1,12 @@
 function find(l, target, start=0, end=null) {
 	end = end == null ? l.length : end
 	let mid_index = Math.round((end-start) / 2) + start
+	console.log(l[mid_index], target)
 	if(start <= end){
 		if(l[mid_index] < target){
-			find(l, target, start = mid_index + 1, end = end)
+			return find(l, target, start = mid_index + 1, end = end)
 		} else if (l[mid_index] > target) {
-			find(l, target, start = 0, end = mid_index - 1)
+			return find(l, target, start = 0, end = mid_index - 1)
 		}else{
 			console.log(mid_index)
 			return mid_index
@@ -16,4 +17,6 @@ function find(l, target, start=0, end=null) {
 }
 let arr = [1,2,45,67,89,100,110,120,150,180,190]
 let result = find(arr, 180)
-console.log(result)
+console.log(result) //undefined
+
+
