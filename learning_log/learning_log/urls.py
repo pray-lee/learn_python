0f26da076c0b29e5d29d8ce82ad0067b20127bd2
pray-lee <praycis@gmail.com>
2019-04-24 22:03:42 +0800

    update

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-
+from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', include('learning_logs.urls'), name='learning_logs'),
+    path(r'', include('learning_logs.urls', namespace='learning_logs'))
 ]
--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -3,8 +3,16 @@ from django.urls import re_path
 
 from . import views
 
+# 如果在learning_log
+# 下的urls.py里面增加了子路由，并且增加了命名空间，这里要再声明一下app_name才不会出现namespace
+# is not registed的错误
+app_name = '[learning_logs]'
 
 urlpatterns = [
     # 主页
-    re_path(r'^$', views.index, name='index')
+    re_path(r'^$', views.index, name='index'),
+    # 主题
+    re_path(r'^topics/$', views.topics, name='topics'),
+    # 主题详情
+    re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
 ]
