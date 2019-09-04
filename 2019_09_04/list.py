01796df261931135c9f3ef1106f9dce1ba27abbe
pray_lee <praycis@gmail.com>
2019-09-04 14:57:51 +0800

    update

---
+++ 2019_09_04/list.py
@@ -0,0 +1,5 @@
+# list列表 / 组   有序
+li = [1, 2, 3, 4, 5]
+print(li[0]) # 返回的还是列表 [1]
+li1 = ['a', 'b']
+print(li + li1) # 和js concat一样。合并列表
---
+++ 2019_09_04/set.py
@@ -0,0 +1,12 @@
+# 无序列表 不会有重复的元素
+set1 = {1, 2, 3, 4, 5}
+
+set2 = {4, 5, 6}
+
+print(set1 - set2) # 求差集 {1,2,3}
+print(set1 & set2) # 求交集 {4,5}
+print(set1 | set2) # 求合集  {1,2,3,4,5}
+
+# 定义一个空集合
+set3 = set()
+print(type(set3), len(set3)) # 'set' 0
